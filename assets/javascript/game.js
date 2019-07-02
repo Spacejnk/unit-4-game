e3cd7ea996b9d77d11fdb9ce16fb526386a02cad
Timits1 <will4f21@yahoo.com>
2019-07-02 05:19:26 -0700

    added images and text to with jquery link

---
+++ assets/javascript/game.js
@@ -0,0 +1,20 @@
+
+
+
+$(document).ready(function(){
+    $("#randNumber").append("<p>Hello World!</p>");
+
+    $("#randNumber").css("background-color", "green");
+    $("#winLoss").css("background-color", "red");
+    $("#winLoss").append("<p>Win</p><p>Losses</p>");
+    
+
+
+
+
+
+
+
+
+
+});
