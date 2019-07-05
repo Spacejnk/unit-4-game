d7cd69dbf333a28f356f8d4ae62c169d2c0a3fe5
Timits1 <will4f21@yahoo.com>
2019-07-05 02:07:52 -0700

    append score board and info to page created image click function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,49 @@
-
+var compChoice = Math.floor((Math.random() * 102) + 19);
+//document.getElementById("randNumber").innerHTML = x;
+var userChoice = Math.floor((Math.random() * 102) + 19);
+var userChoice2 = Math.floor((Math.random() * 102) + 19);
+var userChoice3 = Math.floor((Math.random() * 102) + 19);
+var userChoice4 = Math.floor((Math.random() * 102) + 19);
 
 
 $(document).ready(function(){
-    $("#randNumber").append("<p>Hello World!</p>");
-
+    $("#randNumber").append( compChoice );
     $("#randNumber").css("background-color", "green");
     $("#winLoss").css("background-color", "red");
-    $("#winLoss").append("<p>Win</p><p>Losses</p>");
+    $("#winLoss").append("<p>Win: </p><p>Losses: </p>");
+    $("#scoreInfo").css("background-color", "yellow");
+    $("#scoreInfo").append("<p>Your total score is:</p>");
+    $("#scoreBoard").css("background-color", "blue");
+    $("#scoreBoard").append("<p>score: 0</p>");
+
+
+    $("#emerald1").append("<p>Your total score is:</p>");
+
+    $("#emerald1").click(function() {
+           
+        alert(userChoice); 
+    });
+    $(" #emerald2").click(function() {
+           
+        alert(userChoice2); 
+    });
+    $(" #emerald3").click(function() {
+           
+        alert(userChoice3).load(); 
+    });
+    $("  #emerald4").click(function() {
+           
+        alert(uuserChoice4); 
+    });
+  
+    
+
+    
+
+   
+    
+    
+
     
 
 
