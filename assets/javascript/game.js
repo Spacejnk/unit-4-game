807060bbfb6edfa39d7d3d1d7201463eca53afae
Timits1 <will4f21@yahoo.com>
2019-07-06 00:16:27 -0700

    restructure code logic and function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,76 +1,75 @@
-// mathRand computer choice
-var compChoice = Math.floor((Math.random() * 102) + 19);
+// create variables for all four crystals
+// create variables for wins, losses and totalScore
+// create a function with variable for both mathRandom used for game
+var compRandomNum = function() {
+    return Math.floor((Math.random() * 102) + 19);
+};
+var userClickRandom = function() {
+    return Math.floor(Math.random() * 11 + 1);
+};
+// create a function variable to check if win or if score go over = lose
 
-//document.getElementById("randNumber").innerHTML = x;
 
-// math rand userChoice on click
-var userChoice = Math.floor((Math.random() * 102) + 19);
-var userChoice2 = Math.floor((Math.random() * 102) + 19);
-var userChoice3 = Math.floor((Math.random() * 102) + 19);
-var userChoice4 = Math.floor((Math.random() * 102) + 19);
+
+
+
 
 
 
 // jQuery function for crystal count
 $(document).ready(function(){
-    $("#randNumber").append( compChoice );
+    $("#randNumber").append( compRandomNum );
     $("#randNumber").css("background-color", "green");
     $("#winLoss").css("background-color", "red");
     $("#winLoss").append("<p>Win: </p><p>Losses: </p>");
     $("#scoreInfo").css("background-color", "yellow");
     $("#scoreInfo").append("<p>Your total score is:</p>");
     $("#scoreBoard").css("background-color", "blue");
-    $("#scoreBoard").append(parseInt($("<p>score: </p")));
+    $("#scoreBoard").append(userClickRandom());
 
 
-
-    $(".jewel").click(function() { 
-            var userChoice = parseInt($("#scoreBoard").text());
+                        
            
-            $("#scoreBoard").text((userChoice++));
+
+
+
+
+    
+           
+                $("#emerald1").click(function() {
+                $("#scoreBoard").append(userClickRandom());
+                    console.log(userClickRandom() + " em1");
+                    alert(userClickRandom()); 
+                });
+
             
-
-        });
-
-    $("#emerald1").click(function() {
-            $("#scoreBoard").text(userChoice++);
-                //alert(userChoice); 
-            });
-
-
-  
+                $("#emerald2").click(function() {
+                $("#scoreBoard").append(userClickRandom());
+                     console.log(userClickRandom() + " em2");
+                     alert(userClickRandom()); 
+                 });
 
 
 
-        $(" #emerald2").click(function() {
-            
-            //alert(userChoice2); 
-        });
-
-        $(" #emerald3").click(function() {
-            
-            //alert(userChoice3).load(); 
-        });
-
-        $("  #emerald4").click(function() {
-            
-            //alert(uuserChoice4); 
-        });
-  
-    
 
     
-
-   
-    
-    
-
-    
-
-
-
-
-
-
-
 });
+
+
+
+
+
+
+
+
+
+                // $(" #emerald3").click(function() {
+                        
+                //     //alert(userChoice3).load(); 
+                // });
+
+                //  $("  #emerald4").click(function() {
+                        
+                //     //alert(uuserChoice4); 
+                // });
+            
